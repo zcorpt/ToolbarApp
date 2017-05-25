25cab326dd0dfa648c3cfe0e35a0d0c6df151727
Crono90 <giovanni_sasuke@hotmail.com>
2017-05-24 22:32:01 -0500

    info completo

--- app/src/main/java/com/herprogramacion/toolbarapp/info.java
+++ app/src/main/java/com/herprogramacion/toolbarapp/info.java
@@ -21,7 +21,113 @@ public class info {
                     "Se caracteriza por la presencia leve de escamas blancas o un sarpullido escamoso rojizo o grasoso y amarillento. \n" +
                     "Las zonas comúnmente afectadas son:\n+" +
                     "•\t el cuero cabelludo\n" +
-                    "•\tla parte posterior y exterior de las orejas, las cejas\n"
+                    "•\tla parte posterior y exterior de las orejas, las cejas\n",
+
+
+ /*Cirrosis*/
+
+            "Es una afección en la que el hígado se deteriora lentamente y funciona mal debido a una lesión crónica. El tejido sano del hígado se sustituye por tejido cicatricial, bloqueando parcialmente la circulación de la sangre a través del hígado. La cicatrización también deteriora la capacidad del hígado.",
+
+//DEPRESION//
+
+
+                    "La depresión es una enfermedad mental marcada por sentimientos de tristeza profunda y ausencia de interés en las actividades. La depresión no es lo mismo que la tristeza." +
+                    "La depresión no es lo mismo que la tristeza"+
+                    "Es un estado de ánimo persistentemente depresivo que afecta la capacidad de funcionar y de apreciar los aspectos de la vida." +
+                    "Puede provocar una amplia cantidad de síntomas, tanto físicos como emocionales." +
+                    "Puede durar semanas, meses o años." +
+                    "Las personas con depresión raramente se recuperan sin tratamiento.",
+
+
+/*Diabetis tipo 2*/
+
+            "Es una enfermedad que dura toda la vida (crónica) en la cual hay un alto nivel de azúcar (glucosa) en la sangre",
+
+//ESCLEROSIS MULTIPLE
+
+            "Es una enfermedad autoinmunitaria que afecta el cerebro y la médula espinal (sistema nervioso central).",
+
+// Faringitis
+
+            "La faringitis, o dolor de garganta, es la molestia, el dolor o la carraspera en la garganta que a menudo hace que se presente dolor al tragar.",
+
+ /*Gastritis*/
+
+            "La gastritis ocurre cuando el revestimiento del estómago resulta hinchado o inflamado. Puede durar sólo por un corto tiempo (gastritis aguda). También puede perdurar durante meses o años (gastritis crónica)",
+
+
+/*Gripe*/
+
+            "La gripe es una infección respiratoria causada por cierto número de virus. El virus se transmite por el aire e ingresa al organismo a través de la nariz o la boca.",
+
+   /*HEPATITIS B*/
+            "Es una enfermedad del hígado provocada por el virus llamada hepatitis B."+
+
+            "La mayoría de las infecciones por hepatitis B se resuelven en un lapso de 1 a 2 meses sin tratamiento.\n" +
+                    "Cuando la infección dura más de seis meses, puede tornarse en una hepatitis B crónica, la cual desemboca en:\n" +
+                    "•\tInflamación crónica del hígado\n" +
+                    "•\tCirrosis(cicatrización del hígado)\n" +
+                    "•\tCáncer hepático\n" +
+                    "•\tInsuficiencia hepática\n" +
+                    "•\tMuerte",
+
+//HIPERTENSION ARTERIAL
+            "La presión arterial es una medición de la fuerza ejercida contra las paredes de las arterias a medida que el corazón bombea sangre a su cuerpo. Hipertensión es el término que se utiliza para describir la presión arterial alta.",
+
+// INSOMNIO
+            "Consiste en una reducción de la capacidad para dormir, pudiendo manifestarse de diversos modos que dan lugar a diferentes tipos de insomnio",
+
+//LEUCEMIA
+            "La leucemia es un tipo de cáncer de la sangre que comienza en la médula ósea, el tejido blando que se encuentra en el centro de los huesos, donde se forman las células sanguíneas.",
+
+//MALARIA
+            "El paludismo o malaria es una enfermedad provocada por el parásito Plasmodium, un parásito unicelular, que se transmite a través de la picadura de mosquitos infectados hembra del género Anopheles",
+
+//MIGRAÑA
+            "La migraña es una clase de dolor de cabeza recurrente que afecta los vasos sanguíneos, los nervios y la composición química del cerebro." +
+                    "Antes de la migraña, se pueden experimentar distintas sensaciones, que pueden incluir cambios visuales o entumecimiento y hormigueo, llamadas auras. " +
+                    "Existen dos tipos de migraña:\n" +
+                    "•\tMigraña con aura (anteriormente llamada \"clásica”)\n" +
+                    "•\tMigraña sin aura (anteriormente llamada \"común”)\n" +
+                    "•\tLa migraña pueden afectar el rendimiento escolar del niño, las relaciones con los amigos y la familia, y otros factores en la vida del niño.",
+
+//NEUROPATIA
+            "La neuropatía diabética es un tipo de daño nervioso asociado con la diabetes. Afecta los nervios de los pies, las piernas y los ojos y los nervios que controlan funciones corporales como la digestión, la presión arterial y la frecuencia cardíaca.",
+
+// ORZUELO
+            "Un orzuelo es una glándula sebácea inflamada en el borde del párpado, donde este se encuentra con la pestaña, y aparece como una protuberancia inflamada y rojiza que luce como un grano. Con frecuencia es sensible al tacto.",
+
+//Paperas
+            "Es una enfermedad contagiosa que causa una inflamación dolorosa de las glándulas salivales, las cuales producen la saliva, un líquido que humedece los alimentos y le ayuda a uno a masticar y tragar.",
+
+//PANCREATITIS
+            "a pancreatitis es una enfermedad en donde el páncreas se encuentra inflamado.\n" +
+                    "El páncreas es un órgano largo, liso, con forma de pera, localizado por detrás del estómago. Produce enzimas digestivas y hormonas,\n " +
+                    "incluyendo a la insulina. En la pancreatitis, las enzimas digestivas atacan al tejido que las produce.Pancreatitis aguda: " +
+                    "•\tAparece de repente, con dolor intenso en la parte superior del abdomen. " +
+                    "•\tSi no se trata, esta enfermedad puede ser grave y potencialmente mortal." +
+                    "•\tPancreatitis crónica: trastorno progresivo que puede destruir el páncreas.",
+
+//QUISTES OVARICOS
+            "Un quiste es un saco lleno de líquido que se forma sobre o dentro de un ovario.",
+
+//RESFRIADO
+            "Infección viral común de la nariz y la garganta",
+
+//SIDA
+            "El VIH ocasiona el SIDA y, además, interfiere con la capacidad del cuerpo de combatir infecciones." ,
+
+// TRACOMA
+            "Es una infección ocular causada por una bacteria llamada chlamydia.",
+
+// 	URTICARIA
+            "Son ronchas rojizas, elevadas y a menudo pruriginosas que aparecen en la superficie de la piel. Usualmente son una reacción alérgica a algún alimento o medicamento.",
+
+//Varicela
+            "Es una infección viral por la cual la persona presenta ampollas muy pruriginosas en todo el cuerpo."
+
+
+
 
     };
 
